28ddc40595b8d2b5ac815af900c6e83bf8ad9b6e
debasish pradhan <debasish.pradhan@gmail.com>
2026-03-20 22:44:41 +0530

    Add initial project files and configuration for Python 3.12

--- tests/unit/models/test_lc_platform_provider_open_ai.py
+++ tests/unit/models/test_lc_platform_provider_open_ai.py
@@ -2,8 +2,6 @@ import os
 
 import pytest
 from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
-from langchain_openai import ChatOpenAI
-from pydantic import SecretStr
 
 from backend.ai.llm.chat_models.llm_open_ai import get_llm_chat_open_ai
 from shared.consts import const_config
@@ -74,40 +72,3 @@ def test_chat_model_azure_open_ai(skip, model, options, request):
                 print(f'Response = {block["text"]}')
 
     # get token details
-
-
-def test_chat_open_ai():
-    "Site - ai.azure.com/foundryProject"
-    "Azure OpenAI"
-    # dotenv.load_dotenv(dotenv_path=os.path.join(PARENT_DIR, ".env"))
-    llm = ChatOpenAI(
-        base_url="https://testauthoringagent-resource.openai.azure.com/openai/v1",
-        api_key=SecretStr("6veQnGS1ZLbIeuj9KyyiQdD6Xnc0awP5Z0212MEWQKPoBSLt2okCJQQJ99CAACHYHv6XJ3w3AAAAACOGKX63"),
-        model="gpt-5-chat"
-        # temperature=0,
-        # max_tokens=1600
-
-    )
-    messages = ChatPromptTemplate.from_messages(
-        [SystemMessagePromptTemplate.from_template(template="You Are Helpful Assistant", template_format="f-string"),
-         HumanMessagePromptTemplate.from_template(template="{user_prompt}", template_format="f-string")])
-    chain = messages | llm
-    response = chain.invoke({
-        "user_prompt": """
-                A farmer has 3 sons. He has 17 horses and wants to divide them as follows:
-                - Eldest son gets 1/2 of the horses
-                - Middle son gets 1/3 of the horses
-                - Youngest son gets 1/9 of the horses
-                How can this be done without killing any horse?
-                Show your full reasoning step by step and finally summarize how many each son will get -.
-                Format - 
-                1. Eldest Son - x
-                2. Middle Son - y
-                3. Youngest Son - z
-                where x,y,z are solutions to problem.
-            """  # ← complex puzzle that FORCES reasoning
-    }
-    )
-    assert response.content is not None
-    assert (len(response.content) > 0) is True
-    print(response.content)
